87925c33743098cd00ee7eff38a984b32b2629b3
Antonio Paredes <sirantoniodeparedes@gmail.com>
2026-02-12 15:27:51 +0100

    Lesson 28 Ended

--- 4-Shaders/28 - Shader patterns/src/shaders/test/vertex.glsl
+++ 4-Shaders/28 - Shader patterns/src/shaders/test/vertex.glsl
@@ -1,4 +1,11 @@
+
+//attribute vec2 uv; //Aixo ja no ho hem de posar perque estam a un ShaderMaterial i no un RawShaderMaterial
+
+varying vec2 vUv;
+
 void main()
 {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+    vUv = uv;
 }
