561930650930f6f168b3c55ffcf6280bb82102e6
Antonio Paredes <sirantoniodeparedes@gmail.com>
2026-02-20 01:46:57 +0100

    Lesson 34 ended

--- 4-Shaders/34 - Fireworks/src/script.js
+++ 4-Shaders/34 - Fireworks/src/script.js
@@ -4,6 +4,7 @@ import GUI from 'lil-gui'
 import fireworkVertexShader from './shaders/firework/vertex.glsl'
 import fireworkFragmentShader from './shaders/firework/fragment.glsl'
 import gsap from 'gsap'
+import { Sky } from 'three/examples/jsm/Addons.js'
 
 /**
  * Base
@@ -89,7 +90,7 @@ const createFirework = (count, position, size, texture, radius, color) => {
     //Geometry
     const positionsArray = new Float32Array(count * 3)
     const sizesArray = new Float32Array(count)
-
+    const timeMultiplierArray = new Float32Array(count)
 
     for (let i = 0; i < count; i++) {
         const i3 = i * 3
@@ -107,11 +108,14 @@ const createFirework = (count, position, size, texture, radius, color) => {
         positionsArray[i3 + 2] = position.z
 
         sizesArray[i] = Math.random()
+
+        timeMultiplierArray[i] = 1 + Math.random()
     }
 
     const geometry = new THREE.BufferGeometry()
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(positionsArray, 3))
     geometry.setAttribute('aSize', new THREE.Float32BufferAttribute(positionsArray, 1))
+    geometry.setAttribute('aTimeMultiplier', new THREE.Float32BufferAttribute(timeMultiplierArray, 1))
 
     //Material
     texture.flipY = false //Sino, per alguna extranya rao, three.js gira s'eix y i sa textura se veuria al reves
@@ -167,16 +171,78 @@ createFirework(
     new THREE.Color('#8affff')
 )
 
-window.addEventListener('click', () => {
-    createFirework(
-        1000,                    //Count
-        new THREE.Vector3(),    //Position
-        0.5,                    //Size
-        textures[7],            //Texture
-        1,                      //Radius of the sphere
-        new THREE.Color('#8affff')
+const createRandomFirework = () => {
+    const count = Math.round(400 + Math.random() * 1000)
+    const position = new THREE.Vector3(
+        (Math.random() -0.5) * 2,
+        Math.random(),
+        (Math.random() -0.5) * 2
     )
-})
+    const size = 0.1 + Math.random() * 0.1
+    const texture = textures[Math.floor(Math.random() * textures.length)]
+    const radius = 0.5 + Math.random()
+    const color = new THREE.Color()
+    color.setHSL(Math.random(), 1, 0.7)
+
+    createFirework(
+        count,
+        position,
+        size,
+        texture,
+        radius,
+        color
+    )
+}
+
+window.addEventListener('click', createRandomFirework)
+
+/**
+ * Sky
+ */
+const sky = new Sky()
+sky.scale.setScalar(450000)
+scene.add(sky)
+
+const sun = new THREE.Vector3()
+
+const skyParameters = {
+    turbidity: 10,
+    rayleigh: 3,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.95,
+    elevation: -2.2,
+    azimuth: 180,
+    exposure: renderer.toneMappingExposure
+}
+
+const updateSky = () =>
+{
+    const uniforms = sky.material.uniforms
+    uniforms['turbidity'].value = skyParameters.turbidity
+    uniforms['rayleigh'].value = skyParameters.rayleigh
+    uniforms['mieCoefficient'].value = skyParameters.mieCoefficient
+    uniforms['mieDirectionalG'].value = skyParameters.mieDirectionalG
+
+    const phi = THREE.MathUtils.degToRad(90 - skyParameters.elevation)
+    const theta = THREE.MathUtils.degToRad(skyParameters.azimuth)
+
+    sun.setFromSphericalCoords(1, phi, theta)
+
+    uniforms['sunPosition'].value.copy(sun)
+
+    renderer.toneMappingExposure = skyParameters.exposure
+    renderer.render(scene, camera)
+}
+
+gui.add(skyParameters, 'turbidity', 0.0, 20.0, 0.1).onChange(updateSky)
+gui.add(skyParameters, 'rayleigh', 0.0, 4, 0.001).onChange(updateSky)
+gui.add(skyParameters, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(updateSky)
+gui.add(skyParameters, 'mieDirectionalG', 0.0, 1, 0.001).onChange(updateSky)
+gui.add(skyParameters, 'elevation', 0, 90, 0.1).onChange(updateSky)
+gui.add(skyParameters, 'azimuth', - 180, 180, 0.1).onChange(updateSky)
+gui.add(skyParameters, 'exposure', 0, 1, 0.0001).onChange(updateSky)
+
+updateSky()
 
 /**
  * Animate
--- 4-Shaders/34 - Fireworks/src/shaders/firework/vertex.glsl
+++ 4-Shaders/34 - Fireworks/src/shaders/firework/vertex.glsl
@@ -4,21 +4,50 @@ uniform vec2 uResolution;
 uniform float uProgress;
 
 attribute float aSize;
+attribute float aTimeMultiplier;
 
-float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax){
-    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
-}
+#include ../includes/remap.glsl
 
 void main (){
 
     vec3 newPosition = position;
+    float progress = uProgress * aTimeMultiplier;
+
+    //Exploding
+    float explodingProgress = remap(progress, 0.0, 0.1, 0.0, 1.0); 
+    explodingProgress = clamp(explodingProgress, 0.0, 1.0);
+    explodingProgress = 1.0 - pow(1.0 - explodingProgress, 3.0);
+    newPosition *= explodingProgress;
+
+    //Falling
+    float fallingProgress = remap(progress, 0.1, 1.0, 0.0, 1.0); //Anira desde 0.1 a nes total des temps desde 0 fins a 1
+    fallingProgress = clamp(fallingProgress, 0.0, 1.0);
+    fallingProgress = 1.0 - pow(1.0 - fallingProgress, 3.0);
+    newPosition.y -= fallingProgress * 0.2;
+
+    //Scaling
+    float sizeOpeningProgress = remap(progress, 0.0, 0.125, 0.0, 1.0);
+    float sizeClosingProgress = remap(progress, 0.125, 1.0, 1.0, 0.0);
+    float sizeProgress = min(sizeOpeningProgress, sizeClosingProgress);
+    sizeProgress = clamp(sizeProgress, 0.0, 1.0);
+
+    //Twinkle
+    float twinkleProgress = remap(progress, 0.2, 0.8, 0.0, 1.0);
+    twinkleProgress = clamp(twinkleProgress, 0.0, 1.0);
+    float sizeTwinkle = sin(progress * 30.0) * 0.5 + 0.5;
+    sizeTwinkle = 1.0 - (sizeTwinkle * twinkleProgress);
 
     //Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     gl_Position = projectionMatrix * viewPosition;
 
     //Final size
-    gl_PointSize = uSize * uResolution.y * aSize; //Aixo es perque sa mida des punts tambe canvii segons sa resolucio i no tenguin sempre sa mateixa mida
+    gl_PointSize = uSize * uResolution.y * aSize * sizeProgress * sizeTwinkle; //Aixo es perque sa mida des punts tambe canvii segons sa resolucio i no tenguin sempre sa mateixa mida
     gl_PointSize *= 1.0 / - viewPosition.z; //IMPORTANT: Es signe de negatiu, sino se veu molt molt petit
+
+    if(gl_PointSize < 1.0){
+        gl_Position = vec4(9999.9); //Per algunes GPU ses particules estan clamp a 1.0, aixi que se veuen i renderitzen. Per sol-lucionar-ho,
+                                    //si tenen un tamany mes petit que 1.0, els movem molt enfora i aixi no se renderitzen
+    }
 }
