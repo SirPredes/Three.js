a1ae39c2fa633506e1fb870dd4c397c7823ee6e1
Antonio Paredes <sirantoniodeparedes@gmail.com>
2026-03-03 13:51:40 +0100

    Lesson 39 Ended

--- 4-Shaders/39 - Particles Cursor Animation/src/script.js
+++ 4-Shaders/39 - Particles Cursor Animation/src/script.js
@@ -67,9 +67,83 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(sizes.pixelRatio)
 
 /**
+ * Displacement
+ */
+
+const displacement = {}
+
+//2D canvas
+displacement.canvas = document.createElement('canvas')
+displacement.canvas.width = 128
+displacement.canvas.height = 128
+
+displacement.canvas.style.position = 'fixed'
+displacement.canvas.style.width = '256px'
+displacement.canvas.style.height = '256px'
+displacement.canvas.style.top = 0
+displacement.canvas.style.left = 0
+displacement.canvas.style.zIndex = 10
+document.body.append(displacement.canvas)
+
+//Obtain context
+displacement.context = displacement.canvas.getContext('2d') //Very important to be lowercase
+//displacement.context.fillStyle = 'red' //Important abans de fillRect si volem que canvii es color
+displacement.context.fillRect(0,0, displacement.canvas.width, displacement.canvas.height)
+
+//Glow Image
+displacement.glowImage = new Image()
+displacement.glowImage.src = './glow.png'
+
+//Interactive plane
+displacement.interactivePlane = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshBasicMaterial({
+        color: 'red',
+        side: THREE.DoubleSide,
+    }),
+)
+scene.add(displacement.interactivePlane)
+displacement.interactivePlane.visible = false
+
+//Raycaster
+displacement.raycaster = new THREE.Raycaster()
+
+//Coordinates
+displacement.screenCursor = new THREE.Vector2(9999, 9999) //Aixi no se desplaça nomes obrir sa pagina
+displacement.canvasCursor = new THREE.Vector2(9999, 9999)
+displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999)
+
+window.addEventListener('pointermove', (event) => { //Es com cursormove pero tambe funciona en mobils
+    displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1
+    displacement.screenCursor.y = - (event.clientY / sizes.height) * 2 + 1
+
+    //console.log(event.clientX)
+})
+
+//Texture
+displacement.texture = new THREE.CanvasTexture(displacement.canvas)
+
+/**
  * Particles
  */
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 32, 32)
+const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128)
+particlesGeometry.setIndex(null) //Per simplificar es nombre de vertex que se manden a sa GPU, indexa els vertex de sa geometria
+                                //Aixo normalment es molt bo perque ajuda a s'optimitzacio. 
+                                //Pero ara estam mandant sa geometria a sa mesh de punts i aixo causa que els multipliqui
+                                //Innecessariament. Per llevarli aquesta carrega i que no fiqui 6 vertex (amb es seu punt) a cada
+                                //vertex, hem d'eliminar s'index.
+particlesGeometry.deleteAttribute('normal') //Tambe podem eliminar els atributs que no feim servir
+
+const intensitiesArray = new Float32Array(particlesGeometry.attributes.position.count)
+const anglesArray = new Float32Array(particlesGeometry.attributes.position.count)
+
+for(let i = 0; i < particlesGeometry.attributes.position.count; i++){
+    intensitiesArray[i] = Math.random()
+    anglesArray[i] = Math.random() * Math.PI * 2
+}
+
+particlesGeometry.setAttribute('aIntensity', new THREE.BufferAttribute(intensitiesArray, 1))
+particlesGeometry.setAttribute('aAngle', new THREE.BufferAttribute(anglesArray, 1))
 
 const particlesMaterial = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
@@ -77,6 +151,8 @@ const particlesMaterial = new THREE.ShaderMaterial({
     uniforms:
     {
         uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+        uPictureTexture: new THREE.Uniform(textureLoader.load('./picture-1.png')),
+        uDisplacementTexture: new THREE.Uniform(displacement.texture),
     }
 })
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
@@ -89,6 +165,46 @@ const tick = () =>
 {
     // Update controls
     controls.update()
+
+    /**
+     * Raycaster
+     */
+    displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
+    const intersections = displacement.raycaster.intersectObject(displacement.interactivePlane)
+
+    if(intersections.length){
+        const uv = intersections[0].uv
+        
+        displacement.canvasCursor.x = uv.x * displacement.canvas.width
+        displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height
+    }
+
+    /**
+     * Displacement
+     */
+    displacement.context.globalCompositeOperation = 'source-over'
+    displacement.context.globalAlpha = 0.02
+    displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
+
+    //Speed alpha
+    const cursorDistance = displacement.canvasCursorPrevious.distanceTo(displacement.canvasCursor)
+    displacement.canvasCursorPrevious.copy(displacement.canvasCursor)
+    const alpha = Math.min(cursorDistance * 0.1, 1)
+
+    //Draw glow
+    const glowSize = displacement.canvas.width * 0.25
+    displacement.context.globalCompositeOperation = 'lighten'
+    displacement.context.globalAlpha = alpha
+    displacement.context.drawImage(
+        displacement.glowImage,
+        displacement.canvasCursor.x - glowSize * 0.5,
+        displacement.canvasCursor.y - glowSize * 0.5,
+        glowSize,
+        glowSize
+    )
+
+    //Texture
+    displacement.texture.needsUpdate = true
 
     // Render
     renderer.render(scene, camera)
--- 4-Shaders/39 - Particles Cursor Animation/src/shaders/particles/fragment.glsl
+++ 4-Shaders/39 - Particles Cursor Animation/src/shaders/particles/fragment.glsl
@@ -1,6 +1,18 @@
+
+varying vec3 vColor;
+
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    vec2 uv = gl_PointCoord;
+    float distanceToCenter = distance(uv, vec2(0.5)); //Tambe podria ser length(uv - vec2(0.5))
+
+    if(distanceToCenter > 0.5){
+        discard; //Aalerta perque pot tenir performance impact 
+                //perque quan ha de mirar que esta per davant de que ha de comprobar tots els discards
+                //LLaavors s'ha de vigilar  si n'hi ha molts
+    }
+
+    gl_FragColor = vec4(vColor, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
